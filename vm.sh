733718ed18d4420bd81be21fd5f659dea7064b7a
tumme <tummehr@gmail.com>
2021-12-13 11:01:44 +0100

    t

---
+++ vm.sh
@@ -0,0 +1,7 @@
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+sudo apt install jq -y
+sudo apt install ansible -y
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
